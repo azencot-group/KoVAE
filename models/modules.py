0d3e22fb3173acba551769a1502db4a76d8f657d
ilannaiman <naimani@post.bgu.ac.il>
2024-09-30 14:50:14 +0300

    modules file

---
+++ models/modules.py
@@ -0,0 +1,33 @@
+import torch
+
+class FinalTanh(torch.nn.Module):
+    def __init__(self, input_channels, hidden_channels, hidden_hidden_channels, num_hidden_layers):
+        super(FinalTanh, self).__init__()
+        self.input_channels = input_channels
+        self.hidden_channels = hidden_channels
+        self.hidden_hidden_channels = hidden_hidden_channels
+        self.num_hidden_layers = num_hidden_layers
+
+        self.linear_in = torch.nn.Linear(
+            hidden_channels, hidden_hidden_channels)
+        self.linears = torch.nn.ModuleList(torch.nn.Linear(hidden_hidden_channels, hidden_hidden_channels)
+                                           for _ in range(num_hidden_layers - 1))
+        self.linear_out = torch.nn.Linear(
+            hidden_hidden_channels, input_channels * hidden_channels)
+
+    def extra_repr(self):
+        return "input_channels: {}, hidden_channels: {}, hidden_hidden_channels: {}, num_hidden_layers: {}" \
+               "".format(self.input_channels, self.hidden_channels,
+                         self.hidden_hidden_channels, self.num_hidden_layers)
+
+    def forward(self, z):
+
+        z = self.linear_in(z)
+        z = z.relu()
+        for linear in self.linears:
+            z = linear(z)
+            z = z.relu()
+        z = self.linear_out(z).view(
+            *z.shape[:-1], self.hidden_channels, self.input_channels)
+        z = z.tanh()
+        return z
